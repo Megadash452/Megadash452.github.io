a1fb391ae2260539aad9cac75409fe2ba433cd09
marti <marticraft680@gmail.com>
2020-08-13 20:27:50 -0400

    trying to fix iframes heights to a fraction of screen

--- website.js
+++ website.js
@@ -1,2 +1,8 @@
-/*started JS file. link to HTML/PHP file.*/
-/*webstie bot*/
+const chromeBar = 65;
+const navBar = 54;
+
+const resolution = [screen.width, screen.height];
+const webResolution = [resolution[0], resolution[1] - chromeBar - navBar];
+
+document.getElementsByClassName('main-frame').style.height = `${webResolution[1]}px`;
+document.getElementsByClassName('sidebar-frame').style.height = `${webResolution[1]}px`;
