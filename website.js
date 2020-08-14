56b8e5c833b221dd805fda4c9969bc34828b1d80
marti <marticraft680@gmail.com>
2020-08-14 17:32:46 -0400

    js doms are no more :pensive:

--- website.js
+++
@@ -1,7 +0,0 @@
-const chromeBar = 65;
-const navBar = 54;
-
-const webResolution = [screen.availWidth, screen.availHeight - chromeBar - navBar];
-
-document.getElementsByClassName('main-frame').style.height = `${webResolution[1]}px`;
-document.getElementsByClassName('sidebar-frame').style.height = `${webResolution[1]}px`;
