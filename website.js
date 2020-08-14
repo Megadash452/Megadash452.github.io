ad10ff218d67648deb9fdfda618f589651844054
marti <marticraft680@gmail.com>
2020-08-13 21:12:01 -0400

    no

--- website.js
+++ website.js
@@ -1,8 +1,7 @@
 const chromeBar = 65;
 const navBar = 54;
 
-const resolution = [screen.width, screen.height];
-const webResolution = [resolution[0], resolution[1] - chromeBar - navBar];
+const webResolution = [screen.availWidth, screen.availHeight - chromeBar - navBar];
 
 document.getElementsByClassName('main-frame').style.height = `${webResolution[1]}px`;
 document.getElementsByClassName('sidebar-frame').style.height = `${webResolution[1]}px`;
